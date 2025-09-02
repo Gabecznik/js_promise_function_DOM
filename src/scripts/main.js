9f5327d312a6ffe1f98610645c1b399e3c0b5fad
Marta Osikiewicz <marta.osikiewicz@gmail.com>
2025-09-02 13:25:08 +0200

    task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,11 +1,23 @@
 'use strict';
 
 function waitFor(element, eventName) {
-  // write your code here
+  return new Promise((resolve) => {
+    // tutaj mówisz: "słuchaj, aż wydarzy się event"
+    element.addEventListener(eventName, () => {
+      // kiedy event się stanie → wywołaj resolve
+      resolve(
+        `It was ${eventName} on the element: ${element.nodeName}, id: ${element.id}.`,
+      );
+    });
+  });
 }
 
 const printMessage = (message) => {
-  // write your code here
+  const div = document.createElement('div');
+
+  div.classList.add('message');
+  div.textContent = message;
+  document.body.appendChild(div);
 };
 
 const loginField = document.getElementById('login');
